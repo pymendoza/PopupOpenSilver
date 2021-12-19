01e50b216406e22226052a2557bece9273d37cd7
Peregrino Mendoza <pymendoza@gmail.com>
2021-12-19 23:27:33 +0800

    Initial commit

---
+++ PopupSample/PopupSample/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PopupSample
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+            this.RootVisual = new MainPage();
+
+            //var mainPage = new MainPage();
+            //Window.Current.Content = mainPage;
+        }
+    }
+}
---
+++ PopupSample/PopupSample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PopupSample
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void currentUser_Click(object sender, RoutedEventArgs e)
+        {
+            userActions.IsOpen = !userActions.IsOpen;
+        }
+
+        private void userActions_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Grid_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void userActions_Closed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
